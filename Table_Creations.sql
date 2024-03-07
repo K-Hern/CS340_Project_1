535f046dc412d6e3b2b11eb28ad694fa2aac84df
Kevin Hernandez <KFHEDU@gmail.com>
2024-03-07 09:19:35 -0800

    Updating gitignore and table creation file formatting

--- Table_Creations.sql
+++ Table_Creations.sql
@@ -1,12 +1,8 @@
 --CREATE DATABASE cs_340_p1
 
-
-
 CREATE TYPE charge_type AS ENUM ('annual fee', 'checkin fees');
 CREATE TYPE discount_type AS ENUM ('freebie', 'percentage coupon', 'fixed coupon', 'other');
 CREATE TYPE item_type AS ENUM ('service', 'product');
-
-
 
 CREATE TABLE employee (
   employee_id INT NOT NULL,
@@ -22,10 +18,7 @@ CREATE TABLE employee (
   zip_code VARCHAR(120),
   PRIMARY KEY (employee_id)
   );
---NO REf
-
-
-
+--No Ref
 
 CREATE TABLE company (
   company_id INT NOT NULL,
@@ -36,26 +29,23 @@ CREATE TABLE company (
   url VARCHAR(120),
   PRIMARY KEY (company_id)
   );
---NO REf
-
-
-
+--No Ref
+--Address constraint URL
 
 CREATE TABLE "user"(
- user_id INT NOT NULL,
- user_email VARCHAR(120),
- password VARCHAR(120),
- first_name VARCHAR(120),
- last_name VARCHAR(120),
- street_address VARCHAR(120),
- city VARCHAR(120),
- state VARCHAR(120),
- zip_code VARCHAR(120),
- PRIMARY KEY (user_id)
+  user_id INT NOT NULL,
+  user_email VARCHAR(120),
+  password VARCHAR(120),
+  first_name VARCHAR(120),
+  last_name VARCHAR(120),
+  street_address VARCHAR(120),
+  city VARCHAR(120),
+  state VARCHAR(120),
+  zip_code VARCHAR(120),
+  PRIMARY KEY (user_id)
  );
---NO REf
-
-
+--No Ref
+--Address constraint email
 
 CREATE TABLE item (
   item_id INT NOT NULL,
@@ -67,8 +57,6 @@ CREATE TABLE item (
   PRIMARY KEY (item_id)
   );
 
-
-
 CREATE TABLE discount (
   discount_id INT NOT NULL,
   discount_type discount_type,
@@ -79,9 +67,7 @@ CREATE TABLE discount (
   PRIMARY KEY (discount_id)
   );
 ---Auto updating id
---NO REf
-
-
+--No Ref
 
 CREATE TABLE user_interests (
   user_id INT NOT NULL,
@@ -90,8 +76,6 @@ CREATE TABLE user_interests (
   FOREIGN KEY (user_id)
   REFERENCES "user"(user_id)
 );
-
-
 
 CREATE TABLE user_company_preference (
   user_id INT NOT NULL REFERENCES "user"(user_id),
@@ -103,110 +87,96 @@ CREATE TABLE user_company_preference (
   REFERENCES company(company_id)
 );
 
-
 CREATE TABLE user_item_preference (
-user_id INT NOT NULL,
-item_id INT NOT NULL,
-PRIMARY KEY (user_id, item_id),
-FOREIGN KEY (user_id)
-REFERENCES "user"(user_id),
-FOREIGN KEY (item_id)
-REFERENCES item(item_id)
+  user_id INT NOT NULL,
+  item_id INT NOT NULL,
+  PRIMARY KEY (user_id, item_id),
+  FOREIGN KEY (user_id)
+  REFERENCES "user"(user_id),
+  FOREIGN KEY (item_id)
+  REFERENCES item(item_id)
 );
 
-
 CREATE TABLE user_location_preference (
-user_id INT NOT NULL,
-preference_location_city VARCHAR(120) NOT NULL,
-preference_location_state VARCHAR(120) NOT NULL,
-PRIMARY KEY (user_id, preference_location_city, preference_location_state),
-FOREIGN KEY (user_id)
-REFERENCES "user"(user_id)
+  user_id INT NOT NULL,
+  preference_location_city VARCHAR(120) NOT NULL,
+  preference_location_state VARCHAR(120) NOT NULL,
+  PRIMARY KEY (user_id, preference_location_city, preference_location_state),
+  FOREIGN KEY (user_id)
+  REFERENCES "user"(user_id)
 );
 
 CREATE TABLE company_location (
- company_id INT,
- location_id INT NOT NULL,
- street_address VARCHAR(120),
- city VARCHAR(120),
- state VARCHAR(120),
- zip_code VARCHAR(120),
- phone_nbr VARCHAR(120),
- PRIMARY KEY (location_id),
- FOREIGN KEY (company_id)
- REFERENCES company(company_id)
+  company_id INT,
+  location_id INT NOT NULL,
+  street_address VARCHAR(120),
+  city VARCHAR(120),
+  state VARCHAR(120),
+  zip_code VARCHAR(120),
+  phone_nbr VARCHAR(120),
+  PRIMARY KEY (location_id),
+  FOREIGN KEY (company_id)
+  REFERENCES company(company_id)
  );
 
-
 CREATE TABLE company_item (
-company_id INT NOT NULL,
-item_id INT NOT NULL,
-is_discounted BOOLEAN,
-discount_id INT,
-PRIMARY KEY (company_id, item_id),
-FOREIGN KEY (company_id) REFERENCES company(company_id),
-FOREIGN KEY (item_id) REFERENCES item(item_id),
-FOREIGN KEY (discount_id) REFERENCES discount(discount_id)
+  company_id INT NOT NULL,
+  item_id INT NOT NULL,
+  is_discounted BOOLEAN,
+  discount_id INT,
+  PRIMARY KEY (company_id, item_id),
+  FOREIGN KEY (company_id) REFERENCES company(company_id),
+  FOREIGN KEY (item_id) REFERENCES item(item_id),
+  FOREIGN KEY (discount_id) REFERENCES discount(discount_id)
 );
-
 
 CREATE TABLE user_company_review (
-company_id INT NOT NULL,
-user_id INT NOT NULL,
-rating_score NUMERIC CHECK(MOD((rating_score * 10.0),5.0) = 0),
-comments VARCHAR(120),
-PRIMARY KEY (company_id, user_id),
-FOREIGN KEY (company_id) REFERENCES company(company_id),
-FOREIGN KEY (user_id)REFERENCES "user"(user_id)
-
-
+  company_id INT NOT NULL,
+  user_id INT NOT NULL,
+  rating_score NUMERIC CHECK(MOD((rating_score * 10.0),5.0) = 0),
+  comments VARCHAR(120),
+  PRIMARY KEY (company_id, user_id),
+  FOREIGN KEY (company_id) REFERENCES company(company_id),
+  FOREIGN KEY (user_id)REFERENCES "user"(user_id)
 );
-
 
 CREATE TABLE user_checkin (
-checkin_id INT NOT NULL,
-checkin_date DATE,
-user_id INT,
-company_id INT,
-item_id INT,
-discount_id INT,
-PRIMARY KEY (checkin_id),
-FOREIGN KEY (user_id) REFERENCES "user"(user_id),
-FOREIGN KEY (company_id)REFERENCES company(company_id),
-FOREIGN KEY (item_id)REFERENCES item(item_id),
-FOREIGN KEY (discount_id)REFERENCES discount(discount_id)
+  checkin_id INT NOT NULL,
+  checkin_date DATE,
+  user_id INT,
+  company_id INT,
+  item_id INT,
+  discount_id INT,
+  PRIMARY KEY (checkin_id),
+  FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+  FOREIGN KEY (company_id)REFERENCES company(company_id),
+  FOREIGN KEY (item_id)REFERENCES item(item_id),
+  FOREIGN KEY (discount_id)REFERENCES discount(discount_id)
 );
-
-
-
 
 CREATE TABLE company_transaction (
-transaction_id INT NOT NULL,
-transaction_date DATE,
-charge_type charge_type,
-transaction_amount MONEY,
-surcharge MONEY,
-total_charge MONEY GENERATED ALWAYS AS (transaction_amount + surcharge) STORED,
-is_paid BOOLEAN,
-paid_date DATE,
-company_id INT,
-PRIMARY KEY (transaction_id),
-FOREIGN KEY (company_id)
-REFERENCES company(company_id)
+  transaction_id INT NOT NULL,
+  transaction_date DATE,
+  charge_type charge_type,
+  transaction_amount MONEY,
+  surcharge MONEY,
+  total_charge MONEY GENERATED ALWAYS AS (transaction_amount + surcharge) STORED,
+  is_paid BOOLEAN,
+  paid_date DATE,
+  company_id INT,
+  PRIMARY KEY (transaction_id),
+  FOREIGN KEY (company_id)
+  REFERENCES company(company_id)
 );
 
-
-
-
-
 CREATE TABLE company_transaction_checkin (
-transaction_id INT NOT NULL,
-checkin_id INT,
-checkin_charge MONEY,
-PRIMARY KEY (transaction_id),
-FOREIGN KEY (transaction_id)
-REFERENCES company_transaction(transaction_id),
-FOREIGN KEY (checkin_id)
-REFERENCES user_checkin(checkin_id)
+  transaction_id INT NOT NULL,
+  checkin_id INT,
+  checkin_charge MONEY,
+  PRIMARY KEY (transaction_id),
+  FOREIGN KEY (transaction_id)
+  REFERENCES company_transaction(transaction_id),
+  FOREIGN KEY (checkin_id)
+  REFERENCES user_checkin(checkin_id)
 );
 
