79bd30a5014a7181ee9ed99bfa37b517f7e06a85
Kevin Hernandez <KFHEDU@gmail.com>
2024-03-13 22:15:11 -0700

    Adding corrected trigger handling

--- psql_test_file.sql
+++ psql_test_file.sql
@@ -214,18 +214,17 @@ CREATE TABLE company_transaction_checkin (
  -- -- Item Archive Table -- -- 
 CREATE TABLE item_archive (
   item_primary_key TEXT,
-  item_name TEXT,
+  attr_name TEXT,
   old_value TEXT,
   new_value TEXT,
   update_stamp TIMESTAMP WITH TIME ZONE,
-PRIMARY KEY (item_primary_key),
+PRIMARY KEY (item_primary_key, update_stamp),
 FOREIGN KEY (item_primary_key)
 REFERENCES item(item_id)
 );
 
 
  
-
 ----- Temp_data_tables
 
 -- DROP TABLE temp_user;
@@ -261,15 +260,33 @@ product_category TEXT
 CREATE OR REPLACE FUNCTION archive_item() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-  INSERT INTO 
-    item_archive(item_primary_key, item_name, old_value, new_value, update_stamp)
-    VALUES (new.item_id, new.item_name, old.item_price, new.item_price, CURRENT_TIMESTAMP);
 
-      RETURN new;
+    CASE
+      WHEN old.item_type <> new.item_type THEN
+          INSERT INTO item_archive(item_primary_key, attr_name, old_value, new_value, update_stamp)
+            VALUES (new.item_id, 'item_type', old.item_type, new.item_type, CURRENT_TIMESTAMP);
+
+      WHEN old.item_name <> new.item_name THEN
+          INSERT INTO item_archive(item_primary_key, attr_name, old_value, new_value, update_stamp)
+            VALUES (new.item_id, 'item_name', old.item_name, new.item_name, CURRENT_TIMESTAMP);
+
+      WHEN old.item_description <> new.item_description THEN
+          INSERT INTO item_archive(item_primary_key, attr_name, old_value, new_value, update_stamp)
+            VALUES (new.item_id, 'item_description', old.item_description, new.item_description, CURRENT_TIMESTAMP);
+
+      WHEN old.item_price <> new.item_price THEN
+          INSERT INTO item_archive(item_primary_key, attr_name, old_value, new_value, update_stamp)
+            VALUES (new.item_id, 'item_price', old.item_price, new.item_price, CURRENT_TIMESTAMP);
+            
+      WHEN old.item_picture <> new.item_picture THEN
+          INSERT INTO item_archive(item_primary_key, attr_name, old_value, new_value, update_stamp)
+            VALUES (new.item_id, 'item_picture', old.item_picture, new.item_picture, CURRENT_TIMESTAMP);
+    END CASE;
+
+  RETURN new;
 END;
 $BODY$
 LANGUAGE plpgsql;
-
 
 CREATE TRIGGER add_item_record
 BEFORE UPDATE ON item
@@ -356,5 +373,6 @@ CREATE INDEX discount_type_index ON discount USING HASH (discount_type);
 CREATE INDEX company_transaction_date_index ON company_transaction (transaction_date);
 
 CREATE INDEX company_transaction_charge_type_index ON company_transaction USING HASH (charge_type);
+
 
 
